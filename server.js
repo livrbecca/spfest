1effc51bbc46ca34d678c23e753579f7081e8a93
livrbecca <69110329+livrbecca@users.noreply.github.com>
2021-08-23 20:12:47 +0100

    original access token placing

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const spotifyApi = new SpotifyWebApi({
 });
 //const token = "BQCpA1GjJxL0RZ8cZA_2RW3DMKCrRQJVsOIw4WU77mtaQdgTKqQkLQi12cEgCxc_MFDp9-aHQSFSehhk5UztPnPRsja6mk-nlL_qnpIfQqUAtgl_tVjNc09CJic3bbwZjyE4f69jUVOHnShzOeC7pVmBTw5Z5U5tyMwc1J72NKGq_DfIceRb9YsoKwtXhzki85W0ZqyjDgQaq37saT82JHxdk7ZtIH83-0ZyOfp0qwkQR6vHiAHr5zO9sJ5WBhGKQHtQgf6zoajRADRS1Jf6KhpDVOvGZ3g";
 
-//spotifyApi.setAccessToken(access_token);
+spotifyApi.setAccessToken(access_token);
 
 // credentials are optional
 
@@ -98,7 +98,7 @@ app.get("/callback", (req, res) => {
     .authorizationCodeGrant(code)
     .then((data) => {
       const access_token = data.body["access_token"];
-      spotifyApi.setAccessToken(access_token);
+      
       const refresh_token = data.body["refresh_token"];
       const expires_in = data.body["expires_in"];
 
